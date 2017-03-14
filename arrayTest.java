04918f9fa0cf4ad43ed3b52e2b6dc8c0b2050b17
Ben <benschenkenberger@gmail.com>
2017-03-14 13:18:27 -0700

    add arrayTest: for each loop with an array

---
+++ arrayTest.java
@@ -0,0 +1,11 @@
+
+
+
+public class arrayTest {
+  public static void main(String[] args){
+    String[] theEntireArray = {"zero index", "first index", "second index"};
+    for ( String individualEntry : theEntireArray ) {
+      System.out.println( individualEntry);
+    }
+  }
+}
