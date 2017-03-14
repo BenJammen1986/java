d84709da66896927dbda7bb8118e53d78d3371d3
Ben <benschenkenberger@gmail.com>
2017-03-14 13:50:02 -0700

    add groceryList: practice arrays and for each loop

---
+++ groceryList.java
@@ -0,0 +1,15 @@
+
+public class Test {
+  public static void main(String[] args){
+    String[] myGroceryList = { "apples", "oranges", "burgers", "caviar" };
+    Integer[] groceryItemPrices = { 3, 4, 6, 25 };
+    for ( String groceryItem : myGroceryList ) {
+      System.out.println(groceryItem);
+    }
+    Integer total = 0;
+    for ( Integer price : groceryItemPrices ) {
+      total += price;
+    }
+    System.out.println("Your total shopping bill will be: $" + total);
+  }
+}
